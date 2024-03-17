b0a7f1f55ae20563f148908e1511ce163fc27482
ahessmat <ahessmat@gmail.com>
2024-03-17 13:11:43 -0700

    Updated script

--- setup.sh
+++ setup.sh
@@ -16,6 +16,13 @@ git clone https://github.com/danielmiessler/SecLists.git
 gunzip rockyou.txt.gz
 cd ~
 
+echo "-------------------------------------------------------------------"
+echo "----- Installing VS Code ------"
+echo "-------------------------------------------------------------------"
+
+wget -O code-latest.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+sudo apt install ./code-latest.deb
+rm code-latest.deb
 
 echo "-------------------------------------------------------------------"
 echo "----- Installing Node.js ------"
@@ -31,3 +38,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update && sudo apt-get install nodejs -y
+
+echo "-------------------------------------------------------------------"
+echo "----- Don't forget to install Burpsuite Pro! ------"
+echo "-------------------------------------------------------------------"
